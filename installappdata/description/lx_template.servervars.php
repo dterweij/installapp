ed92009735b31b0469b67ebb5f64fee04df594dd
Danny Terweij <d.terweij@lxcenter.org>
2014-01-13 16:36:18 +0100

    Initial import

---
+++ installappdata/description/lx_template.servervars.php
@@ -0,0 +1,42 @@
+<?php 
+$_SERVER = array (
+  'HTTP_HOST' => '__lx_url',
+  'HTTP_USER_AGENT' => 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.6) Gecko/20040612 Firefox/0.8',
+  'HTTP_ACCEPT' => 'text/xml,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,video/x-mng,image/png,image/jpeg,image/gif;q=0.2,*/*;q=0.1',
+  'HTTP_ACCEPT_LANGUAGE' => 'en-us,en;q=0.5',
+  'HTTP_ACCEPT_ENCODING' => 'gzip,deflate',
+  'HTTP_ACCEPT_CHARSET' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+  'HTTP_KEEP_ALIVE' => '300',
+  'HTTP_CONNECTION' => 'keep-alive',
+  'HTTP_COOKIE' => '',
+  'HTTP_CACHE_CONTROL' => 'max-age=0',
+  'PATH' => '/sbin:/usr/sbin:/bin:/usr/bin:/usr/X11R6/bin',
+  'SERVER_SIGNATURE' => '<address>Apache/2.0.52 (Unix) DAV/2 PHP/5.0.2 Server at myhercules.com Port 80</address>
+',
+  'SERVER_SOFTWARE' => 'Apache/2.0.52 (Unix) DAV/2 PHP/5.0.2',
+  'SERVER_NAME' => 'myhercules.com',
+  'SERVER_ADDR' => '192.168.1.100',
+  'SERVER_PORT' => '80',
+  'REMOTE_ADDR' => '192.168.1.100',
+  'DOCUMENT_ROOT' => '__lx_document_root',
+  'SERVER_ADMIN' => 'postmaster',
+  'SCRIPT_FILENAME' => '__lx_full_script_path',
+  'REMOTE_PORT' => '52341',
+  'GATEWAY_INTERFACE' => 'CGI/1.1',
+  'SERVER_PROTOCOL' => 'HTTP/1.1',
+  'REQUEST_METHOD' => 'GET',
+  'QUERY_STRING' => '',
+  'REQUEST_URI' => '__lx_relative_script_path',
+  'SCRIPT_NAME' => '__lx_relative_script_path',
+  'PHP_SELF' => '__lx_relative_script_path',
+  'argv' => 
+  array (
+  ),
+  'argc' => 0,
+);
+
+$HTTP_SERVER_VARS = $_SERVER;
+$HTTP_POST_VARS = $_POST;
+$HTTP_GET_VARS = $_POST;
+$_REQUEST = $_POST;
+$_GET = $_POST;
